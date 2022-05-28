359f1499a44adf4f2c5a49840b78a580acd2fb4e
ryandchandra <ryandchandra1@gmail.com>
2022-05-28 16:14:46 +0800

    Populate data

--- index.js
+++ index.js
@@ -4,8 +4,39 @@
 const firstUser = {};
 const secondUser = {};
 
+// FIRST USER
+firstUser.name = "Monica";
+firstUser.gender = "Female";
+firstUser.age = 17;
+firstUser.email = "monica@dingdong.com";
+firstUser.favoriteColor = new Set(["Yellow","Pink","White","Purple"]);
+firstUser.isHavePet = true;
+firstUser.education = [
+    { name: "SD 01",  city: "Jakarta",  graduate: 2016 },
+    { name: "SMP 02",  city: "Jakarta",  graduate: 2019 },
+    { name: "SMA 03",  city: "Tangerang" },
+];
+firstUser.favoriteRestaurant = new Set(["Bento","Sushi","Pancake","Eggy","Tempura","Bento","Eggy","Padang","Tteok","Sushi","Sushi"]);
+
+// SECOND USER
+secondUser.name = "Wendy";
+secondUser.gender = "Male";
+secondUser.age = 23;
+secondUser.email = "wendy@dingdong.com";
+secondUser.favoriteColor = new Set(["Blue","Black","Grey"]);
+secondUser.isHavePet = false;
+secondUser.education = [
+    { name: "SD 02",  city: "Jakarta",  graduate: 2010 },
+    { name: "SMP 03",  city: "Bogor",  graduate: 2013 },
+    { name: "SMA 01",  city: "Surabaya", graduate: 2016 },
+    { name: "Universitas Maju", city: "Tangerang" }
+];
+secondUser.favoriteRestaurant = new Set(["Tempura","Bento","Sushi","Pancake","Padang","Katsu","Geprek","Pancake","Eggy"]);
+
 // TODO: Masukkan hasil akhir dari kedua user di sini
 const users = [];
+users.push(firstUser);
+users.push(secondUser);
 
 // ! JANGAN MODIFIKASI LINE DI BAWAH
 function main() {
